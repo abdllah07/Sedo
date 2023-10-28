dca927cd4726d3142c1cd03720d6b05ebda2cd56
abdllah07 <abdallahalhasan2@gmail.com>
2023-10-28 19:45:47 +0300

    add job page update navbar

--- java/jobnavbar.js
+++ java/jobnavbar.js
@@ -1,26 +1,27 @@
+
 $(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 100) {
-        $(".navbar").css("transition" , "0.5s");
-  
-          $(".navbar").css("background" , "rgb(3, 27 , 63)");
-        $(".navbar").css("width" , "100%");
-  
-        }
-  
-        else{
-            $(".navbar").css("background" , "transparent");  	
-        }
-    })
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+      $(".nav").css("transition" , "1s");
+
+	  $(".nav").css("background" , "rgb(3, 27 , 63)");
+      $(".nav").css("width" , "100%");
+
+	}
+
+	else{
+		$(".nav").css("background" , "transparent");  	
+	}
   })
+})
+
 
 
   
 $(function () {
   $("#nav-icon").click(function () {
-    console.log("hi");
-    $("nav").css("background-color", "rgb(3, 27 , 63)")
+    $(".nav").css("background-color", "rgb(3, 27 , 63)");
 
   });
 });
--- java/nav.js
+++ java/nav.js
@@ -23,28 +23,20 @@ window.onscroll = function () {
 
 
 
-$(function () {
-    $("#nav-icon").click(function () {
-      console.log("hi");
-      $("nav").css("background-color", "rgb(3, 27 , 63)")
-
-    });
-  });
-
 
 $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 900) {
-      $(".navbar").css("transition" , "1s");
+      $(".nav").css("transition" , "1s");
 
-	    $(".navbar").css("background" , "rgb(3, 27 , 63)");
-      $(".navbar").css("width" , "100%");
+	    $(".nav").css("background" , "rgb(3, 27 , 63)");
+      $(".nav").css("width" , "100%");
 
 	  }
 
 	  else{
-		  $(".navbar").css("background" , "transparent");  	
+		  $(".nav").css("background" , "transparent");  	
 	  }
   })
 })
